a54a922b188597c1025e350fe9d41bf30002adf6
MadazimovSunnatillo <116077279+MadazimovSunnatillo@users.noreply.github.com>
2022-11-18 14:22:00 +0300

    finish

---
+++ js/app.js
@@ -0,0 +1,24 @@
+alert ("Please, tell us about yourself");
+
+let about = prompt("What's your name?");
+console.log(about);
+let lname = prompt("What's your last name?");
+console.log(lname);
+let age = prompt("How old are you?");
+console.log(age);
+let skills = prompt("What's your skills?");
+console.log(skills);
+
+let person = {
+    fname: "Sunnatillo",
+    lname: "Madazimov",
+    age: "22",
+    skills: ["HTML, SASS, CSS"],
+    place: "Uzbekistan",
+};
+
+console.log(person);
+console.log(about);
+console.log(lname);
+console.log(age);
+console.log(skills);
